439592fd63886ae3b68b1127782931da09edf7e6
Nelson Rodrigues <nbr@Nelsons-MBP.home>
2017-03-05 19:24:58 +0000

    cg: open gl render to texture

--- 14-RenderToTexture/main.cpp
+++ 14-RenderToTexture/main.cpp
@@ -1,4 +1,4 @@
- // Include standard headers
+// Include standard headers
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
@@ -20,7 +20,6 @@ using namespace glm;
 #include <common/controls.hpp>
 #include <common/objloader.hpp>
 #include <common/vboindexer.hpp>
-#include <common/tangentspace.hpp>
 
 int main( void )
 {
@@ -32,15 +31,14 @@ int main( void )
         return -1;
     }
     
-    glfwWindowHint(GLFW_SAMPLES, 1);
+    glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE); // So that glBegin/glVertex/glEnd work
     
     // Open a window and create its OpenGL context
-    window = glfwCreateWindow( 1024, 768, "Tutorial 13 - Normal Mapping", NULL, NULL);
+    window = glfwCreateWindow( 1024, 768, "Tutorial 14 - Render To Texture", NULL, NULL);
     if( window == NULL ){
         fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
         getchar();
@@ -48,6 +46,12 @@ int main( void )
         return -1;
     }
     glfwMakeContextCurrent(window);
+    
+    // We would expect width and height to be 1024 and 768
+    int windowWidth = 1024;
+    int windowHeight = 768;
+    // But on MacOS X with a retina screen it'll be 1024*2 and 768*2, so we get the actual framebuffer size:
+    glfwGetFramebufferSize(window, &windowWidth, &windowHeight);
     
     // Initialize GLEW
     glewExperimental = true; // Needed for core profile
@@ -83,47 +87,30 @@ int main( void )
     glBindVertexArray(VertexArrayID);
     
     // Create and compile our GLSL program from the shaders
-    GLuint programID = LoadShaders( "NormalMapping.vertexshader", "NormalMapping.fragmentshader" );
+    GLuint programID = LoadShaders( "StandardShadingRTT.vertexshader", "StandardShadingRTT.fragmentshader" );
     
     // Get a handle for our "MVP" uniform
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
     GLuint ViewMatrixID = glGetUniformLocation(programID, "V");
     GLuint ModelMatrixID = glGetUniformLocation(programID, "M");
-    GLuint ModelView3x3MatrixID = glGetUniformLocation(programID, "MV3x3");
     
     // Load the texture
-    GLuint DiffuseTexture = loadDDS("diffuse.DDS");
-    GLuint NormalTexture = loadBMP_custom("normal.bmp");
-    GLuint SpecularTexture = loadDDS("specular.DDS");
+    GLuint Texture = loadDDS("uvmap.DDS");
     
     // Get a handle for our "myTextureSampler" uniform
-    GLuint DiffuseTextureID  = glGetUniformLocation(programID, "DiffuseTextureSampler");
-    GLuint NormalTextureID  = glGetUniformLocation(programID, "NormalTextureSampler");
-    GLuint SpecularTextureID  = glGetUniformLocation(programID, "SpecularTextureSampler");
+    GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
     
     // Read our .obj file
     std::vector<glm::vec3> vertices;
     std::vector<glm::vec2> uvs;
     std::vector<glm::vec3> normals;
-    bool res = loadOBJ("cylinder.obj", vertices, uvs, normals);
-    
-    std::vector<glm::vec3> tangents;
-    std::vector<glm::vec3> bitangents;
-    computeTangentBasis(
-                        vertices, uvs, normals, // input
-                        tangents, bitangents    // output
-                        );
+    bool res = loadOBJ("suzanne.obj", vertices, uvs, normals);
     
     std::vector<unsigned short> indices;
     std::vector<glm::vec3> indexed_vertices;
     std::vector<glm::vec2> indexed_uvs;
     std::vector<glm::vec3> indexed_normals;
-    std::vector<glm::vec3> indexed_tangents;
-    std::vector<glm::vec3> indexed_bitangents;
-    indexVBO_TBN(
-                 vertices, uvs, normals, tangents, bitangents,
-                 indices, indexed_vertices, indexed_uvs, indexed_normals, indexed_tangents, indexed_bitangents
-                 );
+    indexVBO(vertices, uvs, normals, indices, indexed_vertices, indexed_uvs, indexed_normals);
     
     // Load it into a VBO
     
@@ -142,16 +129,6 @@ int main( void )
     glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
     glBufferData(GL_ARRAY_BUFFER, indexed_normals.size() * sizeof(glm::vec3), &indexed_normals[0], GL_STATIC_DRAW);
     
-    GLuint tangentbuffer;
-    glGenBuffers(1, &tangentbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, tangentbuffer);
-    glBufferData(GL_ARRAY_BUFFER, indexed_tangents.size() * sizeof(glm::vec3), &indexed_tangents[0], GL_STATIC_DRAW);
-    
-    GLuint bitangentbuffer;
-    glGenBuffers(1, &bitangentbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, bitangentbuffer);
-    glBufferData(GL_ARRAY_BUFFER, indexed_bitangents.size() * sizeof(glm::vec3), &indexed_bitangents[0], GL_STATIC_DRAW);
-    
     // Generate a buffer for the indices as well
     GLuint elementbuffer;
     glGenBuffers(1, &elementbuffer);
@@ -162,21 +139,90 @@ int main( void )
     glUseProgram(programID);
     GLuint LightID = glGetUniformLocation(programID, "LightPosition_worldspace");
     
-    // For speed computation
-    double lastTime = glfwGetTime();
-    int nbFrames = 0;
+    
+    // ---------------------------------------------
+    // Render to Texture - specific code begins here
+    // ---------------------------------------------
+    
+    // The framebuffer, which regroups 0, 1, or more textures, and 0 or 1 depth buffer.
+    GLuint FramebufferName = 0;
+    glGenFramebuffers(1, &FramebufferName);
+    glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
+    
+    // The texture we're going to render to
+    GLuint renderedTexture;
+    glGenTextures(1, &renderedTexture);
+    
+    // "Bind" the newly created texture : all future texture functions will modify this texture
+    glBindTexture(GL_TEXTURE_2D, renderedTexture);
+    
+    // Give an empty image to OpenGL ( the last "0" means "empty" )
+    glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, windowWidth, windowHeight, 0,GL_RGB, GL_UNSIGNED_BYTE, 0);
+    
+    // Poor filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    
+    // The depth buffer
+    GLuint depthrenderbuffer;
+    glGenRenderbuffers(1, &depthrenderbuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, windowWidth, windowHeight);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthrenderbuffer);
+    
+    //// Alternative : Depth texture. Slower, but you can sample it later in your shader
+    //GLuint depthTexture;
+    //glGenTextures(1, &depthTexture);
+    //glBindTexture(GL_TEXTURE_2D, depthTexture);
+    //glTexImage2D(GL_TEXTURE_2D, 0,GL_DEPTH_COMPONENT24, 1024, 768, 0,GL_DEPTH_COMPONENT, GL_FLOAT, 0);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    
+    // Set "renderedTexture" as our colour attachement #0
+    glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, renderedTexture, 0);
+    
+    //// Depth texture alternative :
+    //glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depthTexture, 0);
+    
+    
+    // Set the list of draw buffers.
+    GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
+    glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
+    
+    // Always check that our framebuffer is ok
+    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        return false;
+    
+    
+    // The fullscreen quad's FBO
+    static const GLfloat g_quad_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        -1.0f,  1.0f, 0.0f,
+        -1.0f,  1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        1.0f,  1.0f, 0.0f,
+    };
+    
+    GLuint quad_vertexbuffer;
+    glGenBuffers(1, &quad_vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, quad_vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_quad_vertex_buffer_data), g_quad_vertex_buffer_data, GL_STATIC_DRAW);
+    
+    // Create and compile our GLSL program from the shaders
+    GLuint quad_programID = LoadShaders( "Passthrough.vertexshader", "WobblyTexture.fragmentshader" );
+    GLuint texID = glGetUniformLocation(quad_programID, "renderedTexture");
+    GLuint timeID = glGetUniformLocation(quad_programID, "time");
+    
     
     do{
-        
-        // Measure speed
-        double currentTime = glfwGetTime();
-        nbFrames++;
-        if ( currentTime - lastTime >= 1.0 ){ // If last prinf() was more than 1sec ago
-            // printf and reset
-            printf("%f ms/frame\n", 1000.0/double(nbFrames));
-            nbFrames = 0;
-            lastTime += 1.0;
-        }
+        // Render to our framebuffer
+        glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
+        glViewport(0,0,windowWidth,windowHeight); // Render on the whole framebuffer, complete from the lower left corner to the upper right
         
         // Clear the screen
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -189,8 +235,6 @@ int main( void )
         glm::mat4 ProjectionMatrix = getProjectionMatrix();
         glm::mat4 ViewMatrix = getViewMatrix();
         glm::mat4 ModelMatrix = glm::mat4(1.0);
-        glm::mat4 ModelViewMatrix = ViewMatrix * ModelMatrix;
-        glm::mat3 ModelView3x3Matrix = glm::mat3(ModelViewMatrix);
         glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
         
         // Send our transformation to the currently bound shader,
@@ -198,31 +242,15 @@ int main( void )
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
         glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
         glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewMatrix[0][0]);
-        glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewMatrix[0][0]);
-        glUniformMatrix3fv(ModelView3x3MatrixID, 1, GL_FALSE, &ModelView3x3Matrix[0][0]);
         
-        
-        glm::vec3 lightPos = glm::vec3(0,0,4);
+        glm::vec3 lightPos = glm::vec3(4,4,4);
         glUniform3f(LightID, lightPos.x, lightPos.y, lightPos.z);
         
-        // Bind our diffuse texture in Texture Unit 0
+        // Bind our texture in Texture Unit 0
         glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, DiffuseTexture);
-        // Set our "DiffuseTextureSampler" sampler to user Texture Unit 0
-        glUniform1i(DiffuseTextureID, 0);
-        
-        // Bind our normal texture in Texture Unit 1
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, NormalTexture);
-        // Set our "Normal	TextureSampler" sampler to user Texture Unit 0
-        glUniform1i(NormalTextureID, 1);
-        
-        // Bind our normal texture in Texture Unit 2
-        glActiveTexture(GL_TEXTURE2);
-        glBindTexture(GL_TEXTURE_2D, SpecularTexture);
-        // Set our "Normal	TextureSampler" sampler to user Texture Unit 0
-        glUniform1i(SpecularTextureID, 2);
-        
+        glBindTexture(GL_TEXTURE_2D, Texture);
+        // Set our "myTextureSampler" sampler to user Texture Unit 0
+        glUniform1i(TextureID, 0);
         
         // 1rst attribute buffer : vertices
         glEnableVertexAttribArray(0);
@@ -260,30 +288,6 @@ int main( void )
                               (void*)0                          // array buffer offset
                               );
         
-        // 4th attribute buffer : tangents
-        glEnableVertexAttribArray(3);
-        glBindBuffer(GL_ARRAY_BUFFER, tangentbuffer);
-        glVertexAttribPointer(
-                              3,                                // attribute
-                              3,                                // size
-                              GL_FLOAT,                         // type
-                              GL_FALSE,                         // normalized?
-                              0,                                // stride
-                              (void*)0                          // array buffer offset
-                              );
-        
-        // 5th attribute buffer : bitangents
-        glEnableVertexAttribArray(4);
-        glBindBuffer(GL_ARRAY_BUFFER, bitangentbuffer);
-        glVertexAttribPointer(
-                              4,                                // attribute
-                              3,                                // size
-                              GL_FLOAT,                         // type
-                              GL_FALSE,                         // normalized?
-                              0,                                // stride
-                              (void*)0                          // array buffer offset
-                              );
-        
         // Index buffer
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
         
@@ -298,70 +302,45 @@ int main( void )
         glDisableVertexAttribArray(0);
         glDisableVertexAttribArray(1);
         glDisableVertexAttribArray(2);
-        glDisableVertexAttribArray(3);
-        glDisableVertexAttribArray(4);
-        
-        
-        ////////////////////////////////////////////////////////
-        // DEBUG ONLY !!!
-        // Don't use this in real code !!
-        ////////////////////////////////////////////////////////
         
         
         
-        glMatrixMode(GL_PROJECTION);
-        glLoadMatrixf((const GLfloat*)&ProjectionMatrix[0]);
-        glMatrixMode(GL_MODELVIEW);
-        glm::mat4 MV = ViewMatrix * ModelMatrix;
-        glLoadMatrixf((const GLfloat*)&MV[0]);
+        // Render to the screen
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        // Render on the whole framebuffer, complete from the lower left corner to the upper right
+        glViewport(0,0,windowWidth,windowHeight);
         
+        // Clear the screen
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        glUseProgram(0);
+        // Use our shader
+        glUseProgram(quad_programID);
         
-        // normals
-        glColor3f(0,0,1);
-        glBegin(GL_LINES);
-        for (unsigned int i=0; i<indices.size(); i++){
-            glm::vec3 p = indexed_vertices[indices[i]];
-            glVertex3fv(&p.x);
-            glm::vec3 o = glm::normalize(indexed_normals[indices[i]]);
-            p+=o*0.1f;
-            glVertex3fv(&p.x);
-        }
-        glEnd();
-        // tangents
-        glColor3f(1,0,0);
-        glBegin(GL_LINES);
-        for (unsigned int i=0; i<indices.size(); i++){
-            glm::vec3 p = indexed_vertices[indices[i]];
-            glVertex3fv(&p.x);
-            glm::vec3 o = glm::normalize(indexed_tangents[indices[i]]);
-            p+=o*0.1f;
-            glVertex3fv(&p.x);
-        }
-        glEnd();
-        // bitangents
-        glColor3f(0,1,0);
-        glBegin(GL_LINES);
-        for (unsigned int i=0; i<indices.size(); i++){
-            glm::vec3 p = indexed_vertices[indices[i]];
-            glVertex3fv(&p.x);
-            glm::vec3 o = glm::normalize(indexed_bitangents[indices[i]]);
-            p+=o*0.1f;
-            glVertex3fv(&p.x);
-        }
-        glEnd();
-        // light pos
-        glColor3f(1,1,1);
-        glBegin(GL_LINES);
-        glVertex3fv(&lightPos.x);
-        lightPos+=glm::vec3(1,0,0)*0.1f;
-        glVertex3fv(&lightPos.x);
-        lightPos-=glm::vec3(1,0,0)*0.1f;
-        glVertex3fv(&lightPos.x);
-        lightPos+=glm::vec3(0,1,0)*0.1f;
-        glVertex3fv(&lightPos.x);
-        glEnd();
+        // Bind our texture in Texture Unit 0
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, renderedTexture);
+        // Set our "renderedTexture" sampler to user Texture Unit 0
+        glUniform1i(texID, 0);
+        
+        glUniform1f(timeID, (float)(glfwGetTime()*10.0f) );
+        
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, quad_vertexbuffer);
+        glVertexAttribPointer(
+                              0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+                              3,                  // size
+                              GL_FLOAT,           // type
+                              GL_FALSE,           // normalized?
+                              0,                  // stride
+                              (void*)0            // array buffer offset
+                              );
+        
+        // Draw the triangles !
+        glDrawArrays(GL_TRIANGLES, 0, 6); // 2*3 indices starting at 0 -> 2 triangles
+        
+        glDisableVertexAttribArray(0);
+        
         
         // Swap buffers
         glfwSwapBuffers(window);
@@ -375,14 +354,16 @@ int main( void )
     glDeleteBuffers(1, &vertexbuffer);
     glDeleteBuffers(1, &uvbuffer);
     glDeleteBuffers(1, &normalbuffer);
-    glDeleteBuffers(1, &tangentbuffer);
-    glDeleteBuffers(1, &bitangentbuffer);
     glDeleteBuffers(1, &elementbuffer);
     glDeleteProgram(programID);
-    glDeleteTextures(1, &DiffuseTexture);
-    glDeleteTextures(1, &NormalTexture);
-    glDeleteTextures(1, &SpecularTexture);
+    glDeleteTextures(1, &TextureID);
+    
+    glDeleteFramebuffers(1, &FramebufferName);
+    glDeleteTextures(1, &renderedTexture);
+    glDeleteRenderbuffers(1, &depthrenderbuffer);
+    glDeleteBuffers(1, &quad_vertexbuffer);
     glDeleteVertexArrays(1, &VertexArrayID);
+    
     
     // Close OpenGL window and terminate GLFW
     glfwTerminate();
