96f88464955980b33e9a5cf95763ec0ca4813e6f
Nelson Rodrigues <nbr@Nelsons-MBP.home>
2017-03-05 14:53:22 +0000

    cg: correct some basic shaddings

--- 08-BasicShading/main.cpp
+++ 08-BasicShading/main.cpp
@@ -5,13 +5,14 @@
 
 // Include GLEW
 #include <GL/glew.h>
+
 // Include GLFW
 #include <glfw3.h>
 GLFWwindow* window;
+
 // Include GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-
 using namespace glm;
 
 #include <common/shader.hpp>
@@ -30,7 +31,6 @@ int main( void )
         return -1;
     }
     
-    
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -38,14 +38,13 @@ int main( void )
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
     // Open a window and create its OpenGL context
-    window = glfwCreateWindow( 1024, 768, "Tutorial 08 - Basic Shadding", NULL, NULL);
+    window = glfwCreateWindow( 1024, 768, "Tutorial 08 - Basic Shading", NULL, NULL);
     if( window == NULL ){
         fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
         getchar();
         glfwTerminate();
         return -1;
     }
-    
     glfwMakeContextCurrent(window);
     
     // Initialize GLEW
@@ -82,7 +81,7 @@ int main( void )
     glBindVertexArray(VertexArrayID);
     
     // Create and compile our GLSL program from the shaders
-    GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "TextureFragmentShader.fragmentshader" );
+    GLuint programID = LoadShaders( "StandardShading.vertexshader", "StandardShading.fragmentshader" );
     
     // Get a handle for our "MVP" uniform
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
@@ -99,7 +98,7 @@ int main( void )
     std::vector<glm::vec3> vertices;
     std::vector<glm::vec2> uvs;
     std::vector<glm::vec3> normals;
-    bool res = loadOBJ("cube.obj", vertices, uvs, normals);
+    bool res = loadOBJ("suzanne.obj", vertices, uvs, normals);
     
     // Load it into a VBO
     
@@ -126,21 +125,20 @@ int main( void )
                  normals.size() * sizeof(glm::vec3),
                  &normals[0],
                  GL_STATIC_DRAW);
-  
+    
     // Get a handle for our "LightPosition" uniform
     glUseProgram(programID);
     GLuint LightID = glGetUniformLocation(programID, "LightPosition_worldspace");
     
-    
-    
     do{
+        
         // Clear the screen
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         // Use our shader
         glUseProgram(programID);
         
-        //compute the Model View Projection from keyboard and mouse input
+        // Compute the MVP matrix from keyboard and mouse input
         computeMatricesFromInputs();
         glm::mat4 ProjectionMatrix = getProjectionMatrix();
         glm::mat4 ViewMatrix = getViewMatrix();
@@ -152,7 +150,7 @@ int main( void )
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
         glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
         glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewMatrix[0][0]);
-  
+        
         glm::vec3 lightPos = glm::vec3(4,4,4);
         glUniform3f(LightID, lightPos.x, lightPos.y, lightPos.z);
         
@@ -166,7 +164,7 @@ int main( void )
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
         glVertexAttribPointer(
-                              0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+                              0,                  // attribute
                               3,                  // size
                               GL_FLOAT,           // type
                               GL_FALSE,           // normalized?
@@ -178,8 +176,8 @@ int main( void )
         glEnableVertexAttribArray(1);
         glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
         glVertexAttribPointer(
-                              1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-                              2,                                // size : U+V => 2
+                              1,                                // attribute
+                              2,                                // size
                               GL_FLOAT,                         // type
                               GL_FALSE,                         // normalized?
                               0,                                // stride
@@ -198,8 +196,8 @@ int main( void )
                               (void*)0                          // array buffer offset
                               );
         
-        // Draw the triangle !
-        glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+        // Draw the triangles !
+        glDrawArrays(GL_TRIANGLES, 0, vertices.size() );
         
         glDisableVertexAttribArray(0);
         glDisableVertexAttribArray(1);
@@ -209,21 +207,21 @@ int main( void )
         glfwSwapBuffers(window);
         glfwPollEvents();
         
-        
     } // Check if the ESC key was pressed or the window was closed
-    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == 0 );
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+          glfwWindowShouldClose(window) == 0 );
     
     // Cleanup VBO and shader
     glDeleteBuffers(1, &vertexbuffer);
     glDeleteBuffers(1, &uvbuffer);
     glDeleteBuffers(1, &normalbuffer);
     glDeleteProgram(programID);
-    glDeleteTextures(1, &TextureID);
+    glDeleteTextures(1, &Texture);
     glDeleteVertexArrays(1, &VertexArrayID);
-    
     
     // Close OpenGL window and terminate GLFW
     glfwTerminate();
     
     return 0;
 }
+
